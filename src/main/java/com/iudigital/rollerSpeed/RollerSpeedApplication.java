86af5ddff42b54d4a4764cd8612ee4afd41452c9
Santiago Monsalve <monsalvesantiago582@gmail.com>
2026-08-18 19:01:58 -0500

    commit inicial

---
+++ src/main/java/com/iudigital/rollerSpeed/RollerSpeedApplication.java
@@ -0,0 +1,13 @@
+package com.iudigital.rollerSpeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollerSpeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollerSpeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iudigital/rollerSpeed/RollerSpeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iudigital.rollerSpeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollerSpeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
